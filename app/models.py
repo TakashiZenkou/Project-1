c84a45b0345b6152e52dae81528785b2ad9ec76c
TakashiZenkou <kai879@ymail.com>
2022-03-17 11:35:53 -0500

    Project 1

--- app/models.py
+++ app/models.py
@@ -2,14 +2,14 @@ from . import db
 
 class Property(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(80))
+    title = db.Column(db.String(100))
     numofBed = db.Column(db.Integer)
     numofBath = db.Column(db.Integer)
-    location = db.Column(db.String(80))
+    location = db.Column(db.String(100))
     price = db.Column(db.Numeric(15,2))
-    typeOfHouse = db.Column(db.String(80))
+    typeOfHouse = db.Column(db.String(100))
     description = db.Column(db.String(255))
-    photoName = db.Column(db.String(80))
+    photoName = db.Column(db.String(100))
     
     def __init__(self,title,numofBed,numofBath,location,price,typeOfHouse,description,photoName):
         self.title = title
