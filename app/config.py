ee08214a611a2a1c6e4fc4f5abc322feb8c6e05e
TakashiZenkou <kai879@ymail.com>
2022-03-17 21:41:40 -0500

    Project 1

--- app/config.py
+++ app/config.py
@@ -7,6 +7,6 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgres://jyhdzbpjvoolwm:1d393263eaa8dd808f93288e87178fe2fe746326ee1572b33f31c550d4409aea@ec2-3-222-204-187.compute-1.amazonaws.com:5432/d43rokaj76pnvf').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',).replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
--- app/form.py
+++ app/form.py
@@ -7,10 +7,10 @@ from werkzeug.utils import secure_filename
 
 class PropertyForm(FlaskForm):
     Title = StringField("Property Title", validators=[InputRequired()])
-    NumOfBed = StringField("No. of Rooms", validators=[InputRequired()])
-    NumOfBath = StringField("No. of Bathrooms", validators=[InputRequired()])
+    NumOfBed = StringField("No. of Rooms", validators=[InputRequired(), Regexp("^[0-9]+$")])
+    NumOfBath = StringField("No. of Bathrooms", validators=[InputRequired(),Regexp("^[0-9]+$")])
     Location = StringField("Location",validators=[InputRequired()])
     Price = StringField("Price", validators=[InputRequired()])
     Type = SelectField("Property Type", choices=[('House','House'),('Property','Property')], validators=[InputRequired()])
     Description = TextAreaField("Description", render_kw={"rows": 3, "cols": 4}, validators=[InputRequired()])
-    Photo = FileField(validators=[FileRequired(), FileAllowed(['jpg','png'],'Images only!')])
+    Photo = FileField(validators=[FileRequired(), FileAllowed(['jpg','png','jpeg'],'Images only!')])
